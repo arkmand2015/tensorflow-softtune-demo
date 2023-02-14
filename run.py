65d3eadf0add21cd93f6033b5620dfbc97b9ad48
findmine <armando.cuevas@findmine.com>
2023-02-14 15:32:36 -0500

    Added GCS

--- run.py
+++ run.py
@@ -15,16 +15,29 @@ from keras.applications.imagenet_utils import decode_predictions, preprocess_inp
 from keras.models import Model
 from sklearn.decomposition import PCA
 from scipy.spatial import distance
+from google.oauth2 import service_account
+from google.cloud import storage
+import requests
+from io import BytesIO
+# Create API client.
+credentials = service_account.Credentials.from_service_account_info(
+    st.secrets["gcp_service_account"]
+)
+client = storage.Client(credentials=credentials)
+
 st.title('Tensorflow/Fashion Mnist soft tunning')
 
 with open('features.npy','rb') as f:
     features = np.load(f)
 
-images_path = 'dataset'
+bucket = client.bucket('atx_banana_republic')
+images_path = bucket.list_blobs(prefix='Banana_Republic/Mens')
+
+#images_path = '/home/findmine/Desktop/envatxel/pyimagesearch-ropa/Banana_Republic/Mens'
 image_extensions = ['.jpg', '.png', '.jpeg']   # case-insensitive (upper/lower doesn't matter)
 max_num_images = 10000
 
-images = [os.path.join(dp, f) for dp, dn, filenames in os.walk(images_path) for f in filenames if os.path.splitext(f)[1].lower() in image_extensions]
+images = list(images_path)
 if max_num_images < len(images):
     images = [images[i] for i in sorted(random.sample(xrange(len(images)), max_num_images))]
 
@@ -49,24 +62,16 @@ def get_closest_images(query_image_idx, num_results=5):
 
 def get_concatenated_images(indexes, thumb_height):
     thumbs = []
-    for idx in indexes:
-        img = PIL.Image.open(images[idx])
-        img = img.resize((int(img.width * thumb_height / img.height), thumb_height))
-        thumbs.append(img)
-    concat_image = np.concatenate([np.asarray(t) for t in thumbs], axis=1)
-    return concat_image
+    result_imx = [f'https://storage.cloud.google.com/atx_banana_republic/{images[idx].name}' for idx in indexes]
+    st.image(result_imx,width=300)
+
 
 if st.button('Query aleatorio'):
     query_image_idx = int(len(images) * random.random())
     # do a query on a random image
+    im=f'https://storage.cloud.google.com/atx_banana_republic/{images[query_image_idx].name}'
+    st.image(im,"Query")    
 
     idx_closest = get_closest_images(query_image_idx)
-    query_image = get_concatenated_images([query_image_idx], 300)
     results_image = get_concatenated_images(idx_closest, 200)
-
-    # display the query image
-    im=images[query_image_idx]
-    st.image(im,"Query")
-
-    # display the resulting images
-    st.image(results_image,'Resultado')
+    
